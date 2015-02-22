a9419c3db870642943431ee3cd54494c459e288d
Sam Larison <samlarison@gmail.com>
2015-02-22 16:40:32 -0600

    fixing indentation

--- background.js
+++ background.js
@@ -1,18 +1,17 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if(request.enable){
-			chrome.pageAction.show(sender.tab.id)
-			//pageAction.setIcon closes a pageAction popup that is already showing on Vista, we try to avoid calling this redundantly
-			chrome.pageAction.setIcon({tabId:sender.tab.id, path:{19:'images/icon19.png',38:'images/icon38.png'}}, function(){})
-			sendResponse({});
-		}else if(request.disable){
-			//chrome.pageAction.hide()
-			chrome.pageAction.setIcon({tabId:sender.tab.id, path:{19:'images/inactive/icon19.png',38:'images/inactive/icon38.png'}}, function(){})
-			sendResponse({});
-		}else{
-    	sendResponse({});
-    }
-  });
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if(request.enable){
+		chrome.pageAction.show(sender.tab.id)
+		//pageAction.setIcon closes a pageAction popup that is already showing on Vista, we try to avoid calling this redundantly
+		chrome.pageAction.setIcon({tabId:sender.tab.id, path:{19:'images/icon19.png',38:'images/icon38.png'}}, function(){})
+		sendResponse({});
+	}else if(request.disable){
+		//chrome.pageAction.hide()
+		chrome.pageAction.setIcon({tabId:sender.tab.id, path:{19:'images/inactive/icon19.png',38:'images/inactive/icon38.png'}}, function(){})
+		sendResponse({});
+	}else{
+		sendResponse({});
+	}
+});
 
 if(typeof(localStorage["usageStatistics"])=='undefined')localStorage["usageStatistics"]=true;
 if(typeof(localStorage["shareVideos"])=='undefined')localStorage["shareVideos"]=true;
