21f1ea7ab55f60e5554b11581ba15f826ef634b8
Sam Larison <samlarison@gmail.com>
2016-04-30 19:30:06 -0500

    seek html5 video better

--- vidstick.user.js
+++ vidstick.user.js
@@ -1,7 +1,8 @@
-vidStickBlock: {
-if(document.body.getAttribute('chromeextension-video-tape'))break vidStickBlock;
+videoTapeBlock: {
+if(document.body.getAttribute('chromeextension-video-tape'))break videoTapeBlock;
 var tabid=false;
-vidDropShadow=false;
+var vidDropShadow=false;
+var cssPropsWeModify={'width':'w', 'height':'h', 'top':'t', 'left':'l', 'zIndex':'z', 'position':'p', 'WebkitTransform':'wkt', 'boxShadow':'bxs' }; // a loop added to program
 function _ge(g){
 	return document.getElementById(g);
 }
@@ -70,13 +71,12 @@ function nodeInserted(e){
 	clearTimeout(chkForNodesTimeout);
 	chkForNodesTimeout=setTimeout(checkForNodes,250);
 }
-document.body.addEventListener('DOMNodeInserted', nodeInserted, false);
-document.body.addEventListener('DOMNodeRemoved', nodeInserted, false);
 
 var validNodes=[];
 var topFixed=1000;
 var lasMoveVideo=-1;
 var wasEnabled=false;
+
 
 function checkForNodes(){
 	var testurl=window.location.href;
@@ -126,7 +126,8 @@ function checkForNodes(){
 		var ratio = nodes2[x].clientWidth/nodes2[x].clientHeight;
 		if(nodes2[x].clientHeight < minHeight || ratio < minRatio || ratio > maxRatio ) continue;
 		if(nodes2[x].childNodes.length < 1 && nodes2[x].controls == false){
-			validNodes.push({typ:'html5',elm:nodes2[x].parentNode});//video element without child nodes, must have parent contained controlls, parent container that centers video, who knows what else
+			//validNodes.push({typ:'html5',elm:nodes2[x].parentNode});//video element without child nodes, must have parent contained controlls, parent container that centers video, who knows what else
+			validNodes.push({typ:'html5',elm:nodes2[x]});// always use node itself in case it would detect differently on a different pass
 		}else{
 			validNodes.push({typ:'html5-ctn',elm:nodes2[x]});//video node "contains controlls" as expected.  Neato!
 		}
@@ -139,23 +140,27 @@ function checkForNodes(){
 		validNodes.push({typ:'iframe',elm:nodes3[x]});
 	}
 	
-	var oel, myl, tel, gel, props, lastAboveCnt, aboveCtr = 0; // reset counter
+	var oel, elType, myl, tel, gel, props, lastAboveCnt, aboveCtr, foundControlls;
 
 	if(validNodes.length > 0){
 		
 		//now we will loop through the valid nodes and see if we really should control the parent node instead
 		for(var x=0,l=validNodes.length;x<l;x++){
+			elType = validNodes[x].typ;
 			oel=myl=gel=tel=validNodes[x].elm;
 
 			props = myl.getAttribute('vidtapeorigprops');
 
 			lastAboveCnt = myl.getAttribute('vidtapeabovecount') - 0; // how many elements above is our final element
+			aboveCtr = 0;
+
+			console.log( 'got lastAboveCnt', lastAboveCnt);
 
 			while(!props && aboveCtr < lastAboveCnt){
-				tel=myl.parentNode;
+				tel=tel.parentNode;
 				props = tel.getAttribute('vidtapeorigprops');
 
-				if(myl.scrollWidth >= tel.scrollWidth && myl.scrollHeight >= tel.scrollHeight){
+				if( isContainerProxyFor(elType, tel, myl) ){
 					gel = tel;
 				}
 
@@ -165,61 +170,66 @@ function checkForNodes(){
 			if( props && gel != tel ){
 				console.log('pretty odd, we were seeking ', tel, ' but on the way found ', gel,' was a better match - but not sure why we are checking');
 			}
+
 			if( props ){
 				validNodes[x].elm = tel;
+				// its possible there is yet another parent node that is yet a better match now, due to some change in DOM ..
+				// or that it is impossible to get here from video node now
+				// call resetOrigionalProperties(tel)
+				// if we unfix a video though... it should reset and re-detection may occur
 			}
 
+			console.log('has props', props);
+
 			if(!props){
+				aboveCtr = 0;
 
 				// if html5 we might try seeking controlls first, since aspect ratio of video may not match player
 
 				tel=myl.parentNode;
-				while(myl.scrollWidth >= tel.scrollWidth && myl.scrollHeight >= tel.scrollHeight){
+				while( tel && isContainerProxyFor(elType, tel, myl) ){ // reasonalbe q: why search when non html5 elType
 					validNodes[x].elm = gel = tel;
+					aboveCtr++;
 					tel=tel.parentNode;
+
+					//console.log(aboveCtr, myl.scrollWidth , tel.scrollWidth , myl.scrollHeight, tel.scrollHeight);
+					if(elType=='html5'){
+						foundControlls = gel.querySelectorAll('[class*=play],[class*=pause],[class*=mute],[class*=volume],[class*=fullscreen],[class*=time],[class*=duration]');
+						if( foundControlls.length > 3 ){
+							console.log('found controls', foundControlls);
+							tel = null; // dont advance further than necessary when seeking containers
+						}
+					}
 				}
 				// not just class but more attributes might match
 				//$$('[class*=play],[class*=pause],[class*=mute],[class*=volume],[class*=fullscreen],[class*=time],[class*=duration]')
 
-				if( gel == myl ){ // hmm not getting anywhere
-					if(validNodes[x].typ=='html5'){
-						//there is "probably" a controls div.. use the parent instead.. (in some cases this will be the parent of parent node)
-						// along the way up though, if we look at sibling nodes of elm, we may find elements that appear to be controls
-						// and we know once we have the controls we may not need to traverse much farther
-						// the ratio here is a hack
-						// while the container should have a client height it does not always seem to have one... and in this case we may again look to parentNode
-						// tel=tel.parentNode;
-						while(myl.scrollWidth >= tel.scrollWidth && myl.scrollHeight * 1.2 >= tel.scrollHeight){ // we should only resume size search once we know we have a good start point...
-							validNodes[x].elm = tel;//it probably has as larger height
-							//if(tel.getAttribute('vidtapeorigprops'))break;
-							tel=tel.parentNode;
-						}
-					}
-				}
+				// if( gel == myl ){ // hmm not getting anywhere
+				// 	console.log('true to not getting anywhere!')
+				// 	if(validNodes[x].typ=='html5'){
+				// 		//there is "probably" a controls div.. use the parent instead.. (in some cases this will be the parent of parent node)
+				// 		// along the way up though, if we look at sibling nodes of elm, we may find elements that appear to be controls
+				// 		// and we know once we have the controls we may not need to traverse much farther
+				// 		// the ratio here is a hack
+				// 		// while the container should have a client height it does not always seem to have one... and in this case we may again look to parentNode
+
+				// 		// tel=tel.parentNode; // this is sort of alredy set above
+				// 		while( isContainerProxyFor(elType, tel, myl) ){ // we should only resume size search once we know we have a good start point...
+				// 			validNodes[x].elm = gel = tel;//it probably has as larger height
+				// 			aboveCtr++;
+				// 			//if(tel.getAttribute('vidtapeorigprops'))break;
+				// 			tel=tel.parentNode;
+				// 		}
+				// 	}
+				// }
 			}
 
 			myl=validNodes[x].elm;
 			if(!myl.getAttribute('vidtapeorigprops'))
-				myl.setAttribute("vidtapeorigprops",JSON.stringify({w:myl.scrollWidth,h:myl.scrollHeight}));//,typ:validNodes[x].typ
+				myl.setAttribute("vidtapeorigprops",JSON.stringify(getCssPropsWeModify(myl)));//,typ:validNodes[x].typ // {w:myl.scrollWidth,h:myl.scrollHeight}
 			oel.setAttribute('vidtapeabovecount', aboveCtr);
+			console.log( 'setting aboveCtr', aboveCtr);
 		}
-		
-		//lets try to fix all the zindexes too... what a mess! (we just want to place a given element on top of all)... well this block sort of fixes youtube but not good enough
-//		for(var x=0,l=validNodes.length;x<l;x++){
-//			tel=validNodes[x].elm.parentNode;
-//			while(tel.style){
-//				var newz=((tel.style.zIndex ? tel.style.zIndex : 100)-0)+1;
-//				tel.style.zIndex = newz;
-//				//so ugly
-//				var ps=tel.previousSibling;
-//				while(ps){
-//					ps.style.zIndex = ++newz;
-//					ps=ps.previousSibling;
-//				}
-//				
-//				tel=tel.parentNode;
-//			}
-//		}
 		
 		//console.log(validNodes);
 		if(!wasEnabled)
@@ -235,7 +245,48 @@ function checkForNodes(){
 	}
 }
 
-checkForNodes();
+function getCssPropsWeModify(node){
+	var robj={};
+	var prop, propShortKey;
+	for( prop in cssPropsWeModify ){
+		robj[cssPropsWeModify[prop]] = node.style[prop];
+	} // NOTE: we need to reset style.position no matter what, so adding tests in this loop should take care
+	return robj;
+}
+
+function isContainerProxyFor(elType, container, origional){
+
+	var th = container.offsetHeight, hzCropRatio = 1.0, vertCropRatio = 1.042;
+
+	if( th == 0 ){
+		th = container.scrollHeight;
+	}
+	if( elType=='html5' ){
+		hzCropRatio = 1.5;
+	}
+
+	return origional.scrollWidth * hzCropRatio >= container.scrollWidth && origional.scrollHeight * vertCropRatio >= th;
+
+}
+
+function resetOrigionalProperties(validNode){
+	var origProps=JSON.parse(validNode.getAttribute("vidtapeorigprops")) || false;
+	if( !origProps ){
+	}
+	var prop, propShortKey;
+	for( prop in cssPropsWeModify ){
+		propShortKey = cssPropsWeModify[prop];
+		if( typeof origProps[propShortKey] != 'undefined' ) validNode.style[prop] = origProps[propShortKey];
+	}
+	validNode.removeAttribute("vidtapeorigprops")
+	validNode.querySelector('[vidtapeabovecount]').removeAttribute('vidtapeabovecount');
+}
+
+setTimeout(function(){
+	checkForNodes();
+	document.body.addEventListener('DOMNodeInserted', nodeInserted, false);
+	document.body.addEventListener('DOMNodeRemoved', nodeInserted, false);
+}, 1230);
 
 function viewScrolled(){
 	if(tabid)
@@ -271,9 +322,13 @@ function computeBoxShadow(m){
 function affixVideo(i){
 	document.body.style.overflow="scroll"; // in case video ever end up outside of window
 	var m = validNodes[i].elm;
+	if( !m || !m.getAttribute("vidtapeorigprops") ){  console.log('problem affixing');return m;} // TODO: this validation should apply elsewhere, earlier, and hopefully once
+
+	// quite arguably, rather than looking at vidtapeorigprops for values. NOW is the time we should SET those properties (before we make changes) since the "orig" props may haev changed
+
 	var sp=getFixedOffset(m);
-	//m.style.width=(m.clientWidth)+'px !important';
-	//m.style.height=(m.clientHeight)+'px !important';
+	m.style.width=(m.scrollWidth)+'px';
+	m.style.height=(m.scrollHeight)+'px';
 	m.style.position='fixed';
 	m.style.top=(sp.y)+'px';
 	m.style.left=(sp.x)+'px';
@@ -287,7 +342,7 @@ function affixVideo(i){
 	computeBoxShadow(m);
 	//-webkit-transform: translate3d(0px, 0px, 0px);
 	if(!m.getAttribute('domDetached')){
-		var spa=Cr.elm('div',{'class':'_vidstickspacer','events':[['contextmenu',spacerContextMenu]],'style':'display:inline-block;height:'+m.clientHeight+'px;width:'+m.clientWidth+'px;'});
+		var spa=Cr.elm('div',{'class':'_videotapespacer','events':[['contextmenu',spacerContextMenu]],'style':'display:inline-block;height:'+m.clientHeight+'px;width:'+m.clientWidth+'px;'});
 		m.parentNode.insertBefore(spa,m);
 		Cr.addListeners();
 	}
@@ -295,19 +350,13 @@ function affixVideo(i){
 }
 function unfixVideo(i, showRestored){
 	var m = validNodes[i].elm;
-	if(m.previousSibling && m.previousSibling.className=='_vidstickspacer'){
+	if( !m || !m.getAttribute("vidtapeorigprops") ){  console.log('problem unfixing');return m;}
+	if(m.previousSibling && m.previousSibling.className=='_videotapespacer'){
 		m.parentNode.removeChild(m.previousSibling);
 	}
-	var origProps=JSON.parse(m.getAttribute("vidtapeorigprops"));
-	m.style.position='relative';
-	m.style.top='0px';
-	m.style.left='0px';
-	m.style.width=origProps.w+'px';
-	m.style.height=origProps.h+'px';
-	m.style.WebkitTransform='none';
-	m.style.boxShadow='';
+	resetOrigionalProperties(m);
 	if(showRestored)m.scrollIntoViewIfNeeded();
-	viewScrolled();
+	checkForNodes();//viewScrolled();//similar effects, but since we reset node, scan for nodes again
 	return m;
 }
 
@@ -319,7 +368,7 @@ function(request, sender, sendResponse) {
 		for(var i=0,l=validNodes.length;i<l;i++){
 			var m = validNodes[i].elm;
 			var sp=getFixedOffset(m);
-			response.push({x:sp.x,y:sp.y,w:m.clientWidth,h:m.clientHeight,fixed:m.style.position=='fixed'});
+			response.push({x:sp.x,y:sp.y,w:m.scrollWidth,h:m.scrollHeight,fixed:m.style.position=='fixed'});
 		}	
 		sendResponse({win:{w:getWindowWidth(),h:getWindowHeight(),scrypcnt:window.pageYOffset/(document.body.scrollHeight-getWindowHeight()),docHei:document.body.scrollHeight,scry:window.pageYOffset},elm:response});
 	}else if (typeof(request.scrToYpcnt)!='undefined'){
